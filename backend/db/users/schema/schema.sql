981f39d8a9757088b8b1cf2d4d4ada8b60de5ed9
Turbobot <turbobot@vercel.com>
2023-12-03 12:17:21 +0100

    feat(create-turbo): apply official-starter transform

--- backend/db/users/schema/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    username TEXT  PRIMARY KEY,
-    password TEXT NOT NUll,
-    email TEXT UNIQUE NOT NULL,
-    token TEXT,
-    verification_token TEXT,
-    verified INTEGER
-);
