3cc5f5ec2e40f6ae5a013edd58d4c99e090a6036
Strahinja <strahinja2001@gmail.com>
2023-12-06 00:55:51 +0100

    added graphql

--- backend/server.go
+++ backend/server.go
@@ -1,8 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"Knoxianes/fairesults/graph"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+)
+
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 func main() {
-    router := gin.Default()
-    router.Run(":3000")
+	router := gin.Default()
+	router.POST("/query", graphqlHandler())
+	router.GET("/", playgroundHandler())
+
+	router.Run(":3000")
+
 }
